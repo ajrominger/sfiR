12c0d811b6208b0a557e51a9e8e08ac01bea03e9
ajrominger <ajrominger@gmail.com>
2018-05-30 17:10:13 -0600

    the sfi colors

---
+++ R/sfiColors.R
@@ -0,0 +1,5 @@
+sfiColors <- c('#D68F85', '#005D77', '#51661A', '#D49A34', 
+               '#58455F', '#AF2F23', '#008E94', '#A27635', 
+               '#636051', '#DECEA1', '#D15A2A', '#CCCEC8', '#322B29')
+
+plot(1:length(sfiColors), col = sfiColors, pch = 16, cex = 2)
