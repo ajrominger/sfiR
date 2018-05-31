d01073bf8b6241a60858d05e5b7c273649ecedc7
ajrominger <ajrominger@gmail.com>
2018-05-31 11:31:51 -0600

    added nice looking sets of colors

--- R/sfiColors.R
+++ R/sfiColors.R
@@ -10,3 +10,20 @@ m <- persp(range(foo[, 2]), range(foo[, 3]), matrix(range(foo[, 1]), nrow = 2, n
            theta = 0, phi = 90)
 points(trans3d(foo[, 2], foo[, 3], foo[, 1], pmat = m), col = sfiColors, pch = 16, cex = 2)
 text(trans3d(foo[, 2], foo[, 3], foo[, 1], pmat = m), labels = 1:length(sfiColors), col = 'white', cex = 0.5)
+
+
+# different color sets
+warm <- c(1, 4, 6, 8, 10, 11, 13)
+cool <- c(2, 3, 5, 7, 9, 12, 13)
+bigGradient <- c(2, 7, 3, 8, 4, 11, 6)
+divergent1 <- c(7, 12, 11)
+divergent2 <- c(2, 12, 4)
+divergent3 <- c(2, 12, 8)
+linear1 <- c(7, 9, 11)
+linear2 <- c(7, 13, 6)
+linear3 <- c(2, 9, 8)
+linear4 <- c(2, 9, 4)
+linear5 <- c(2, 12, 10)
+linear6 <- c(7, 10, 4)
+lilGradient1 <- c(5, 6, 11, 4)
+lilGradient2 <- c(5, 2, 7, 3)
